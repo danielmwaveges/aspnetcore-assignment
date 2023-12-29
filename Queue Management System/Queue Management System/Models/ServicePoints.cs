214528653747191fd530bb9b4adee1fa0e28417e
danielmwaveges <danielmwaveges@gmail.com>
2023-12-29 10:56:32 +0300

    delete service point and service providers models

--- Queue Management System/Queue Management System/Models/ServicePoints.cs
+++
@@ -1,8 +0,0 @@
-namespace Queue_Management_System.Models
-{
-    public class ServicePoints{
-        public string ServicePoint;
-        public string ServiceDescription;
-        public string PassKey;
-    }
-}
